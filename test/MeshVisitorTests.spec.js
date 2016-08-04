0130e24705aac98b39476cd0bff3dc80708cc4ab
Lotes <markhoernchen@gmx.de>
2016-08-04 22:25:35 +0200

    add mesh visitor

---
+++ test/MeshVisitorTests.spec.js
@@ -0,0 +1,26 @@
+const nodeThree = require('../lib/node-three/index')
+const THREE = nodeThree.THREE
+const path = require('path')
+const Promise = require('bluebird')
+const fs = require('fs')
+const meshVisit = require('../lib/meshVisit')
+
+describe('Mesh visitor', function() {
+  const url = path.join(__dirname, 'data/cube/cube.obj')
+  var mesh
+
+  before(function() {
+    return nodeThree.loadOBJ(url)
+      .then(function(object) {
+        mesh = object
+      })
+  })
+
+  it('should visit all 12 triangles of the cube', function() {
+    var count = 0
+    meshVisit(mesh, function(triangle) {
+      count++
+    })
+    count.should.be.equal(12)
+  })
+})
