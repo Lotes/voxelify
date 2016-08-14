72672674b0439f48b6357c092ff212432dcb8401
Lotes <markhoernchen@gmx.de>
2016-08-14 16:11:56 +0200

    optimize fetching image data

--- test/VoxelGridTests.spec.js
+++ test/VoxelGridTests.spec.js
@@ -6,8 +6,19 @@ const Vector3 = THREE.Vector3
 const VoxelGrid = require('../lib/VoxelGrid')
 const Face = require('../lib/Face')
 const Vertex = require('../lib/Vertex')
+const path = require('path')
 
 describe('VoxelGrid', function() {
+  const url = path.join(__dirname, 'data/cube/cube.png')
+  var material
+
+  before(function() {
+    return nodeThree.loadTexture(url)
+      .then(function(texture) {
+        material = texture
+      })
+  })
+
   it('should split face leafing an empty voxel', function() {
     /*
          | X
@@ -17,7 +28,6 @@ describe('VoxelGrid', function() {
        XX|XX
     */
     const grid = new VoxelGrid(new Box3(new Vector3(0, 0, 0), new Vector3(2, 2, 1)))
-    const material = new MeshBasicMaterial({ color: 0xff0000 })
     const a = new Vertex([0, 0, 0], [0, 0])
     const b = new Vertex([2, 0, 0], [1, 0])
     const c = new Vertex([2, 2, 0], [1, 1])
