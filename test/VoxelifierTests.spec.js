9843b60168a71c4603af75b11f0c30647fc122cb
Lotes <markhoernchen@gmx.de>
2017-02-16 19:06:40 +0100

    implementing first command

--- test/VoxelifierTests.spec.js
+++ test/VoxelifierTests.spec.js
@@ -9,13 +9,14 @@ const path = require('path')
 const fs = require('fs')
 const GIFEncoder = require('gifencoder')
 const Promise = require('bluebird')
+const THREE = ThreeRenderExtensions.THREE
 
 describe('Voxelifier', function () {
   const url = path.join(__dirname, 'data/dk2/DolDonkeykongR1.obj') // 'data/venusaur/Venusaur.obj'
   const RESULTS_DIRECTORY = path.join(__dirname, 'results')
   const size = 40
 
-  var object
+  let object
 
   before(function () {
     return ThreeLoaderExtensions.loadOBJ(url)
@@ -27,20 +28,14 @@ describe('Voxelifier', function () {
   it('should render all slices of given mesh', function () {
     this.timeout(40000)
     const fileName = path.join(RESULTS_DIRECTORY, 'voxel_map.png')
-    const voxelifier = new Voxelifier({
-      object: object,
-      size: size
-    })
+    const voxelifier = new Voxelifier(object, new THREE.Quaternion(), size, true)
     const colorGrid = voxelifier.compute()
     return colorGrid.save(fileName)
   })
 
   it('should render voxelified mesh', function () {
     this.timeout(40000)
-    const voxelifier = new Voxelifier({
-      object: object,
-      size: size
-    })
+    const voxelifier = new Voxelifier(object, new THREE.Quaternion(), size, true)
     const colorGrid = voxelifier.compute()
     const mesh = colorGrid.toMesh()
     return ThreeRenderExtensions.normalizeSize(mesh)
@@ -48,7 +43,7 @@ describe('Voxelifier', function () {
         return ThreeRenderExtensions.captureByCamera(normalizedMesh, 600, 400)
       })
       .then(function (scene) {
-        var out = fs.createWriteStream(path.join(RESULTS_DIRECTORY, 'voxel_mesh.png'))
+        let out = fs.createWriteStream(path.join(RESULTS_DIRECTORY, 'voxel_mesh.png'))
         return scene.renderToStream(out)
       })
   })
@@ -57,10 +52,7 @@ describe('Voxelifier', function () {
     this.timeout(40000)
     const WIDTH = 600
     const HEIGHT = 400
-    const voxelifier = new Voxelifier({
-      object: object,
-      size: size
-    })
+    const voxelifier = new Voxelifier(object, new THREE.Quaternion(), size, true)
     const colorGrid = voxelifier.compute()
     const mesh = colorGrid.toMesh()
     return ThreeRenderExtensions.normalizeSize(mesh)
@@ -68,12 +60,12 @@ describe('Voxelifier', function () {
         return ThreeRenderExtensions.captureByCamera(mesh, WIDTH, HEIGHT)
       })
       .then(function (scene) {
-        var angles = []
-        for (var angle = 0; angle < 360; angle += 20) {
+        let angles = []
+        for (let angle = 0; angle < 360; angle += 20) {
           angles.push(angle)
         }
 
-        var encoder = new GIFEncoder(WIDTH, HEIGHT)
+        let encoder = new GIFEncoder(WIDTH, HEIGHT)
         encoder.createReadStream().pipe(fs.createWriteStream(path.join(RESULTS_DIRECTORY, 'voxel_animated.gif')))
 
         encoder.start()
@@ -81,9 +73,9 @@ describe('Voxelifier', function () {
         encoder.setDelay(50)  // frame delay in ms
         encoder.setQuality(10) // image quality. 10 is default.
         return Promise.each(angles, function (angle) {
-          var rad = angle / 180 * Math.PI
-          var cos = Math.cos(rad)
-          var sin = Math.sin(rad)
+          let rad = angle / 180 * Math.PI
+          let cos = Math.cos(rad)
+          let sin = Math.sin(rad)
           scene.camera.set(cos, 0, sin)
           return scene.renderToStream().then(function (context) {
             encoder.addFrame(context)
